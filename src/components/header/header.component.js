7aee7fcb3dbbfc3bb563d7cb2a434337ee165815
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-10-10 17:22:27 -0400

    added react recharts

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -7,7 +7,7 @@ const Header = () => {
     return(
         <Box component={'div'} >
             <Typography variant='h1' align='center'> ta<span className="title-bold">X</span>ulator </Typography>
-            <Typography component='h2' variant='h4' align='center' sx={{ color: 'rgba(70, 70, 180, 0.8)' }}> A tax calculator for Canadians </Typography>
+            <Typography component='h2' variant='h4' align='center' sx={{ color: 'rgba(70, 70, 180, 0.8)' }}> A Tax Calculator for Canadians</Typography>
         </Box>
     )
 }
--- src/components/tax-viewer/tax-viewer.component.js
+++ src/components/tax-viewer/tax-viewer.component.js
@@ -1,9 +1,54 @@
 import { Typography, Box } from "@mui/material";
 
 const TaxViewer = () => {
+    const textBoxSx = { width: '25ch', marginY: 1, display: 'flex', justifyContent: 'space-between' }
+    
     return(
         <Box sx={{ p: 3, m: 3, boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px', borderRadius: 4 }}>
-            <Typography variant='h6' align='center'>Results</Typography>
+            <Typography variant='h6' align='center'>Your Results</Typography>
+
+            <Box sx={{ p: 3, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
+
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >Total Income:</Typography>
+                    <Typography variant='body' align='center' >$100,000</Typography>
+                </Box>
+
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >Federal Tax:</Typography>
+                    <Typography variant='body' align='center' >$100,000</Typography>
+                </Box>
+
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >Provincial Tax:</Typography>
+                    <Typography variant='body' align='center' >$100,000</Typography>
+                </Box>
+
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >CPP/EI Premiums:</Typography>
+                    <Typography variant='body' align='center' >$100,000</Typography>
+                </Box>
+
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >Total Tax:</Typography>
+                    <Typography variant='body' align='center' >$100,000</Typography>
+                </Box>
+
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >Average Tax Rate:</Typography>
+                    <Typography variant='body' align='center' >20%</Typography>
+                </Box>
+
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >Marginal Tax Rate:</Typography>
+                    <Typography variant='body' align='center' >50%</Typography>
+                </Box>
+                
+                <Box sx={textBoxSx}>
+                    <Typography variant='body' align='center' >After-tax Income:</Typography>
+                    <Typography variant='body' align='center' >$100,000</Typography>
+                </Box>
+            </Box>
         </Box>
         
     )
